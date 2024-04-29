4ded2c738164cea1ffd7cabb6df286c29aea21b6
Zackiel1 <alejandrozfm132@gmail.com>
2024-04-29 17:16:29 -0300

    nav medio estructurado

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,18 +1,39 @@
 import style from './NavBar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+
 
 const NavBar = () => {
 
     return(
         <main className={style.navContainer}>
+
             <section className={style.photoSection}>
                 {/* <img src={foto} alt="" /> */}
             </section>
+
+            <section className={style.navSection}>
+                <ul className={style.navList}>
+                    <li>Inicio</li>
+                    <li>Experiencia</li>
+                    <li>Proyetos</li>
+                    <li>Quien Soy</li>
+                    <li>Contact</li>
+                </ul>
+            </section>
+
             <section className={style.contactSection}>
-                <h3>Contacto</h3>
+                <div className={style.iconSection}>
+                    <FontAwesomeIcon className={style.icon} icon={faSquareGithub} />
+                    <FontAwesomeIcon className={style.icon} icon={faLinkedin} />
+                    <span className={style.cv}>CV</span>
+                </div>
+                <span>Alejandrozfm132@gmail.com</span>
             </section>
-            <section className={style.NavSection}>
-                <h3>Nav</h3>
-            </section>
+
+           
+            
         </main>
     )
 };
